8db473b8878cae4b89c3abdacfde6b1736013312
Saurab Dulal <dulal.saurab@gmail.com>
2017-10-20 20:16:05 -0500

    UDP client and server file added

---
+++ UDPPingerClient.py
@@ -0,0 +1,56 @@
+'''
+'Client Program'
+@Author: Saurab Dulal 
+ Date. : October 16, 2017 
+ Dependencies: Python 3+ 
+ Description: Simple UDPPingerClient program  	
+'''
+
+import random
+import socket
+import time 
+
+#since we need to send a ping message to specific ip and port 
+#we need to specify servers IP and port number 
+
+UDP_IP = '127.0.0.1' #local host 
+UDP_PORT = 12000 #server listening at port 
+
+serverAddress = (UDP_IP,UDP_PORT)
+clientSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
+
+clientSocket.settimeout(20) #wait for 20 seconds before time out 
+
+try:
+
+	difftime = 0 
+	rcv_flag = False
+	
+	for apptempt in range(0,10):	#sending 10 sucessive message 
+
+		# message format, sequence number and time 
+		MESSAGE  = str("Sequence number :"+str(apptempt)+" and send at time :"+str(time.time()))
+		MESSAGE = MESSAGE.encode('utf-8') #converting message to bytes
+		sending_time = time.time()
+
+		sent = clientSocket.sendto(MESSAGE, serverAddress)
+		
+		try:
+			message, address = clientSocket.recvfrom(1024)
+		except Exception as e:
+			print("Connection time out")
+			continue 
+		end_time = time.time()
+
+		if message:
+			message = message.decode('utf-8').split('token')
+			print("Message from server: "+str(message[0]))
+			print("Receved at :"+str(message[1]))
+			print("Round trip time (RTT):"+str(end_time - sending_time))
+			rcv_flag = False
+
+finally:
+	clientSocket.close()
+
+
+
---
+++ UDPPingerServer.py
@@ -0,0 +1,52 @@
+
+'''
+'Server Program'
+@Author: Saurab
+ Date. : October 16, 2017 
+ Dependencies: Python 3+ 
+ Description:
+
+
+'''
+
+
+
+import random
+import socket 
+import datetime
+
+
+
+	# Create a UDP socket
+	# Notice the use of SOCK_DGRAM for UDP packets 
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# Assign IP address and port number to socket  
+serverAddress = ('127.0.0.1',12000)
+serverSocket.bind(serverAddress) #only used in case of server 
+
+
+while True:
+	
+	print("Server waiting for new request")
+	# Generate random number in the range of 0 to 10
+	rand = random.randint(0, 10)
+	message, address = serverSocket.recvfrom(1024)
+
+	# Receive the client packet along with the address it is coming from 
+	# Capitalize the message from the client
+	# If rand is less is than 4, we consider the packet lost and do not respond 
+	if rand < 4:			
+		continue
+	
+	print(rand)
+	print("Received message from the client: "+str(message.decode('utf-8'))+" and the address is :"+ str(address))
+
+	if message:
+		#Token is used to break the server message at client side
+		message = message.upper()+b'token'+str(datetime.datetime.now()).encode('utf-8') 
+		sent = serverSocket.sendto(message, address)
+	
+
+
+
+
